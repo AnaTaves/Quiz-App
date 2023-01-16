37472e49dc2b1e03d9304015fe2831c796d0de85
Ana Tavares <ana.tavares@smartdata.science>
2023-01-16 13:28:18 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Quiz App
+
+This project was made with vite and react. It is a WIP.
+
+
+# How to run this project
+
+```
+pnpm install
+pnpm dev
+```
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  
+
+  return (
+    <div className="bg-black h-screen">
+     <h1 className='text-center text-neutral-400'>Quiz App</h1>
+    </div>
+  )
+}
+
+export default App
